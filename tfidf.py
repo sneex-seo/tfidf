8be76ef2be8875c402194291be7f085461566466
sneex-seo <80849226+sneex-seo@users.noreply.github.com>
2021-10-20 19:14:50 +0300

    Update tfidf.py

--- tfidf.py
+++ tfidf.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 import yake
+import csv
 
 with open('text.txt', 'r+', encoding='utf-8') as f:
     keywords = [line.strip() for line in f]
 
+def csv_writer(data):
+    with open('result.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        spamWriter = csv.writer(csvfile)
+        spamWriter.writerow(data)
 
 text = " ".join(keywords)
 
+#пишем языковой код языка
 language = "ru"
-max_ngram_size = 1 #какие словосочетания выводить, униграммы (1 слово), биграммы (2 слова), 3 слова и т.д
+#какие словосочетания выводить, униграммы (1 слово), биграммы (2 слова), 3 слова и т.д
+max_ngram_size = 1
 deduplication_thresold = 0.9
-deduplication_algo = 'seqm'
+deduplication_algo = 'se    qm'
 windowSize = 1
 numOfKeywords = 20
 
@@ -21,4 +28,5 @@ dedupLim=deduplication_thresold, dedupFunc=deduplication_algo,
 keywords = custom_kw_extractor.extract_keywords(text)
 
 for kw in keywords:
-    print(kw)
+    result = [kw[0],kw[1]]
+    csv_writer(result)
